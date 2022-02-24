b6bdfc027b7c0484191b765299d83454eab02336
GabrielLSobreira <gabriellaranjasobreira@gmail.com>
2022-02-24 13:17:02 -0300

    feat(home): consumindo os dados do prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,33 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 import Image from 'next/image';
 import techsImage from '../../public/images/techs.svg';
+import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
-export default function Home() {
+interface Response {
+  [key: string]: any;
+}
+
+type Content = {
+  title: string;
+  titleContent: string;
+  linkAction: string;
+  mobileTitle: string;
+  mobileContent: string;
+  mobileBanner: string;
+  webTitle: string;
+  webContent: string;
+  webBanner: string;
+};
+
+interface ContentProps {
+  content: Content;
+}
+
+export default function Home({ content }: ContentProps) {
   return (
     <>
       <Head>
@@ -73,3 +97,42 @@ export default function Home() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.Predicates.at('document.type', 'home'),
+  ]);
+
+  const {
+    title,
+    sub_title,
+    link_action,
+    mobile,
+    mobile_content,
+    mobile_banner,
+    title_web,
+    web_content,
+    web_banner,
+  }: Response = response.results[0].data;
+
+  const content = {
+    title: RichText.asText(title),
+    titleContent: RichText.asText(sub_title),
+    linkAction: link_action.url,
+    mobileTitle: RichText.asText(mobile),
+    mobileContent: RichText.asText(mobile_content),
+    mobileBanner: mobile_banner.url,
+    webTitle: RichText.asText(title_web),
+    webContent: RichText.asText(web_content),
+    webBanner: web_banner.url,
+  };
+
+  return {
+    props: {
+      content,
+    },
+    revalidate: 60 * 2, // A cada 2 minutos
+  };
+};
